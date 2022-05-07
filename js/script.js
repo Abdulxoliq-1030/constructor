dbaf2419170732717535e087fa190f9bf0e1cbd7
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-05-07 16:49:40 +0500

    home section finish code

--- js/script.js
+++ js/script.js
@@ -35,3 +35,10 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   contactInfo.classList.remove("active");
 };
+
+var swiper = new Swiper(".home-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
